6fc709191eaf5274898d484ff1b074a539ece959
Net-Set <somilsomil25@gmail.com>
2024-10-25 11:25:27 +0530

    update calling function X3Grid

--- src/app/retro/program/x3/page.tsx
+++ src/app/retro/program/x3/page.tsx
@@ -1,6 +1,10 @@
 // src/app/retro/program/x3/page.tsx
-import LevelCard from '@/components/LevelCard/x3Card/X3Grid';
+import X3Grid from '@/components/LevelCard/x3Card/X3Grid'; // Assuming X3Grid is the correct import
 
 export default function X3Page() {
-  return <LevelCard />;
+  return (
+    <div>
+      <X3Grid />
+    </div>
+  );
 }
