20105fff846d8ce1b6fa53deee5f7043b8f6fdc5
rajveer9720 <rajveersingh05021999@gmail.com>
2024-11-16 17:59:48 +0530

    Resolve fetchContract Data

--- src/components/landingpage/components/ActivitySection.tsx
+++ src/components/landingpage/components/ActivitySection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { fetchPartnerData, Partner } from '@/components/SmartContract/smartcontract';
+import {fetchPartnerData, Partner } from '@/components/parteners/smartcontract/smartcontract';
 
 const PartnerPage: React.FC = () => {
   const [partners, setPartners] = useState<Partner[]>([]);
