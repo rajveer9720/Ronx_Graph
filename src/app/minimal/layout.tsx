4d8c7f3429090c1c1be726f4fb6a987c392e062c
rajveer9720 <rajveersingh05021999@gmail.com>
2024-12-03 16:12:16 +0530

    minimal page fiex

--- src/app/minimal/layout.tsx
+++ src/app/minimal/layout.tsx
@@ -1,17 +1,12 @@
 import MinimalLayout from '@/layouts/minimal/layout';
-import { WalletProvider } from '@/app/context/WalletContext';
+import { WalletProvider } from  '@/app/context/WalletContext';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <WalletProvider>
-      {typeof window === 'undefined' ? (
-        // Fallback rendering for SSR
-        <MinimalLayout>
-          <div>Loading...</div>
-        </MinimalLayout>
-      ) : (
+     
         <MinimalLayout>{children}</MinimalLayout>
-      )}
+     
     </WalletProvider>
   );
 }
--- src/components/ui/accountpreview.tsx
+++ src/components/ui/accountpreview.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const AccountPreview: React.FC = () => {
   const [inputValue, setInputValue] = useState('');
