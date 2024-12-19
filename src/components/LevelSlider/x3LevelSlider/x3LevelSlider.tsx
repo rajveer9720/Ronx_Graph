b8bcb8bf55b234650efed3a54bff59d0075bc7b8
Net-Set <somilsomil25@gmail.com>
2024-12-20 00:25:36 +0530

    cycle way data fetch in console log

--- src/components/LevelSlider/x3LevelSlider/x3LevelSlider.tsx
+++ src/components/LevelSlider/x3LevelSlider/x3LevelSlider.tsx
@@ -13,7 +13,6 @@ import { x3Activelevelpartner, GET_REGISTRATIONS } from '@/graphql/level_Ways_Pa
 import { GET_WALLET_ADDRESS_TO_ID } from '@/graphql/WalletAddress_To_Id/queries';
 import { GET_WALLET_ADDRESS_TO_UPLINE_ID } from '@/graphql/WalletAddress_To_UplineId/queries';
 
-
 const levels = [
   { level: 1, cost: 0.0001 },
   { level: 2, cost: 0.0002 },
@@ -87,7 +86,6 @@ const LevelSliderx3: React.FC = () => {
     fetchUplineId();
   }, [staticAddress]);
 
-
   useEffect(() => {
     const fetchData = async () => {
       if (!staticAddress) return;
@@ -112,19 +110,51 @@ const LevelSliderx3: React.FC = () => {
         levels.map((level) =>
           client.query({
             query: x3Activelevelpartner,
-            variables: { walletAddress: staticAddress, level: level.level },
+            variables: { walletAddress: "0xD733B8fDcFaFf240c602203D574c05De12ae358C", level: level.level },
           })
         )
       );
 
-      const partnerCountsArray = partnersResponse.map((response) => {
+      const partnerCountsArray = partnersResponse.map((response, index) => {
         const partnersAtLevel = response.data.newUserPlaces || [];
+        
+        // Log the user addresses and place values for each partner at this level
+        const userPlaces = partnersAtLevel.map((partner: { user: string; place: number }) => ({
+          user: partner.user,
+          place: partner.place,
+        }));
+        console.log(`Level ${index + 1} User Places:`, userPlaces);
+      
+        // Group users into cycles
+        const cycles = [];
+        for (let i = 0; i < userPlaces.length; i += 3) {
+          const cycle = userPlaces.slice(i, i + 3);
+          cycles.push(cycle);
+        }
+      
+        // Log the cycles
+        cycles.forEach((cycle, cycleIndex) => {
+          console.log(`Cycle ${cycleIndex + 1}`);
+            cycle.forEach((partner: { user: string }, partnerIndex: number) => {
+            console.log(`User ${partnerIndex + 1} Wallet Address: ${partner.user}`);
+            });
+        });
+      
         return partnersAtLevel.length;
       });
 
-      const cycleData = partnerCountsArray.map((partnerCount) => {
+      const cycleData = partnerCountsArray.map((partnerCount, index) => {
         const fullCycles = Math.floor(partnerCount / 3);
         const remainder = partnerCount % 3;
+
+        // Log wallet addresses involved in the cycle way
+        if (fullCycles > 0) {
+          const levelPartners = partnersResponse[index].data.newUserPlaces.map(
+            (partner: { user: string }) => partner.user
+          );
+          console.log(`Level ${index + 1} Cycle Wallet Addresses:`, levelPartners);
+        }
+
         return { fullCycles, remainder };
       });
 
@@ -133,32 +163,31 @@ const LevelSliderx3: React.FC = () => {
       setPartnerCounts(partnerCountsArray);
       setActiveLevels(activeLevelsArray);
 
-            // Now, let's compare each level's partners to direct partners
-            const { data: directPartnersData } = await client.query({
-              query: GET_REGISTRATIONS,
-              variables: { referrer: staticAddress },
-            });
-      
-            const directPartners = directPartnersData.registrations.map(
-              (registration: { user: string }) => registration.user
-            );
-            console.log("Direct Partners:", directPartners);
-      
-            // Check each level's partners against direct partners
-            const actualPartnersPerLevel = partnerCounts.map((_, index) => {
-              const levelPartners = partnersResponse[index].data.newUserPlaces.map(
-                (partner: { user: string }) => partner.user
-              );
-                const uniqueLevelPartners = Array.from(new Set(levelPartners)) as string[];
-                const matchingPartners: string[] = uniqueLevelPartners.filter((partner: string) =>
-                  directPartners.includes(partner)
-                );
-                console.log(`Level ${index + 1} Actual Partners:`, matchingPartners.length);
-                return matchingPartners.length;
-              });
-              
-            setActualPartnersPerLevel(actualPartnersPerLevel);
+      // Now, let's compare each level's partners to direct partners
+      const { data: directPartnersData } = await client.query({
+        query: GET_REGISTRATIONS,
+        variables: { referrer: "0xD733B8fDcFaFf240c602203D574c05De12ae358C" },
+      });
 
+      const directPartners = directPartnersData.registrations.map(
+        (registration: { user: string }) => registration.user
+      );
+      console.log("Direct Partners:", directPartners);
+
+      // Check each level's partners against direct partners
+      const actualPartnersPerLevel = partnerCounts.map((_, index) => {
+        const levelPartners = partnersResponse[index].data.newUserPlaces.map(
+          (partner: { user: string }) => partner.user
+        );
+        const uniqueLevelPartners = Array.from(new Set(levelPartners)) as string[];
+        const matchingPartners: string[] = uniqueLevelPartners.filter((partner: string) =>
+          directPartners.includes(partner)
+        );
+        console.log(`Level ${index + 1} Actual Partners:`, matchingPartners.length);
+        return matchingPartners.length;
+      });
+
+      setActualPartnersPerLevel(actualPartnersPerLevel);
     };
 
     fetchData();
@@ -178,9 +207,8 @@ const LevelSliderx3: React.FC = () => {
         {/* Previous Level Button */}
         <button
           onClick={() => handleLevelChange('prev')}
-          className={`p-4 rounded-3xl h-20 lg:h-24 transition-all duration-200 ease-in-out ${
-            currentLevel === 1 ? 'bg-gray-600 cursor-not-allowed' : 'bg-gray-700 hover:bg-gray-600'
-          }`}
+          className={`p-4 rounded-3xl h-20 lg:h-24 transition-all duration-200 ease-in-out ${currentLevel === 1 ? 'bg-gray-600 cursor-not-allowed' : 'bg-gray-700 hover:bg-gray-600'
+            }`}
           disabled={currentLevel === 1}
         >
           {currentLevel > 1 ? currentLevel - 1 : ''}
@@ -202,11 +230,10 @@ const LevelSliderx3: React.FC = () => {
                   {Array.from({ length: 3 }).map((_, i) => (
                     <div key={i} className="flex flex-col items-center">
                       <div
-                        className={`relative w-24 h-24 rounded-full ${
-                          activeLevels[currentLevel - 1] && i < reminderData[currentLevel - 1]
+                        className={`relative w-24 h-24 rounded-full ${activeLevels[currentLevel - 1] && i < reminderData[currentLevel - 1]
                             ? 'bg-blue-600'
                             : 'bg-gray-400'
-                        }`}
+                          }`}
                       >
                         {i < partnerCounts[currentLevel - 1] && (
                           <span className="absolute inset-0 flex justify-center items-center text-sm text-white">
@@ -221,10 +248,10 @@ const LevelSliderx3: React.FC = () => {
                 {/* Level Stats */}
                 <div className="flex justify-between items-center mb-4">
                   <div className="flex items-center">
-                  <span className="mr-2">👥</span> {actualPartnersPerLevel[currentLevel - 1]}
+                    <span className="mr-2">👥</span> {actualPartnersPerLevel[currentLevel - 1]}
                   </div>
                   <div className="flex items-center">
-                    <span className="mr-2">🔄</span> {cyclesData[currentLevel - 1]} 
+                    <span className="mr-2">🔄</span> {cyclesData[currentLevel - 1]}
                   </div>
                 </div>
 
@@ -238,7 +265,7 @@ const LevelSliderx3: React.FC = () => {
               {/* Inactive Overlay */}
               {!activeLevels[currentLevel - 1] && (
                 <div className="absolute inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center text-white text-lg font-bold">
-                    <button className="px-6 py-2 rounded text-xl">Inactive</button>
+                  <button className="px-6 py-2 rounded text-xl">Inactive</button>
                 </div>
               )}
             </div>
@@ -248,9 +275,8 @@ const LevelSliderx3: React.FC = () => {
         {/* Next Level Button */}
         <button
           onClick={() => handleLevelChange('next')}
-          className={`p-4 rounded-3xl h-20 lg:h-24 transition-all duration-200 ease-in-out ${
-            currentLevel === 12 ? 'bg-gray-600 cursor-not-allowed' : 'bg-gray-700 hover:bg-gray-600'
-          }`}
+          className={`p-4 rounded-3xl h-20 lg:h-24 transition-all duration-200 ease-in-out ${currentLevel === 12 ? 'bg-gray-600 cursor-not-allowed' : 'bg-gray-700 hover:bg-gray-600'
+            }`}
           disabled={currentLevel === 12}
         >
           {currentLevel < 12 ? currentLevel + 1 : ''}
